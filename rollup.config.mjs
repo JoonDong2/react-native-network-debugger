5b47bc240abba82c42622bc0be2692e5f0492117
joondong <kwonjoondong@gmail.com>
2026-02-03 16:54:01 +0900

    fix: 트리쉐이킹 제거

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,17 +1,17 @@
-import { defineConfig } from 'rollup';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import external from 'rollup-plugin-peer-deps-external';
-import json from '@rollup/plugin-json'; 
-import { terser } from 'rollup-plugin-terser';
+import { defineConfig } from "rollup";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import external from "rollup-plugin-peer-deps-external";
+import json from "@rollup/plugin-json";
+import { terser } from "rollup-plugin-terser";
 
 export default defineConfig([
   {
-    input: './src/server/index.js',
+    input: "./src/server/index.js",
     output: [
       {
-        file: './dist/server/index.js',
-        format: 'cjs',
+        file: "./dist/server/index.js",
+        format: "cjs",
         sourcemap: false,
       },
     ],
@@ -29,11 +29,11 @@ export default defineConfig([
     },
   },
   {
-    input: './src/client/index.js',
+    input: "./src/client/index.js",
     output: [
       {
-        file: './dist/client/index.js',
-        format: 'cjs',
+        file: "./dist/client/index.js",
+        format: "cjs",
         sourcemap: false,
       },
     ],
@@ -46,8 +46,5 @@ export default defineConfig([
       commonjs(),
       terser(),
     ],
-    treeshake: {
-      moduleSideEffects: false,
-    },
-  }
+  },
 ]);
